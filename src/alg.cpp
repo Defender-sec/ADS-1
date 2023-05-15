75a3587623b9f0c4fd2110dd2398f245b72bbe5d
Misha Filchagin <79020757+Defender-sec@users.noreply.github.com>
2023-05-15 23:01:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,15 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <iostream>
 #include "alg.h"
-
-
+using namespace std;
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  for (i = 2; i <= sqrt(value); i++) {
+    if (value % i == 0) {
+      return true;
+    }else{
+      return false;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
